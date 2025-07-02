0fac1d1fcd11d0b2306ca69e978b623225357b0d
Ayush-014 <llayush1i1l@gmail.com>
2025-07-02 12:56:51 +0530

    updated user dashboard

--- Frontend/src/components/index.components.js
+++ Frontend/src/components/index.components.js
@@ -2,6 +2,7 @@ import Navbar from "./Common/Navbar.jsx"
 import Footer from "./Common/Footer.jsx"
 import ActivityCards from "./User/ActivityCards.jsx"
 import HeroSection from "./User/HeroSection.jsx"
+import RaftCard from "./RaftCard.jsx"
 
 export {
     Navbar,
