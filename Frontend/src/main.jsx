91399cbc61c5f4bd58ec62faab986f97eaf27e5f
Ayush-014 <llayush1i1l@gmail.com>
2025-07-02 23:24:43 +0530

    user routes aren't protected

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -14,35 +14,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          {/* Public Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
-          {/* Protected User Route (Only the root / dashboard) */}
-          <Route
-            path="/"
-            element={
-              <PrivateRoute allowedRoles={["user", "admin"]} />
-            }
-          >
-            <Route element={<App />}>
-              <Route index element={<UserDashboard />} />
-            </Route>
+          <Route path="/" element={<App />}>
+            <Route index element={<UserDashboard />} />
           </Route>
 
-          {/* Protected Admin Routes */}
-          <Route
-            path="/admin"
-            element={
-              <PrivateRoute allowedRoles={["admin"]} />
-            }
-          >
+          <Route path="/admin" element={<PrivateRoute allowedRoles={["admin"]} />}>
             <Route element={<App />}>
               <Route index element={<AdminDashboard />} />
             </Route>
           </Route>
 
-          {/* Catch-all Route */}
           <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </AuthProvider>
