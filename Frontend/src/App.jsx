bbc6b141ba4142f24d9e5b8ad280b64e216c720f
Harshit_Rawat <110838418+HarshitRawat2@users.noreply.github.com>
2025-07-08 13:39:29 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Navbar, Footer } from "./components/index.components.js"
 import { Outlet } from 'react-router-dom';
 import "./index.css"
+import ScrollToTop from "./components/User/ScrollToTop.js";
 function App() {
   return (
     <div className="min-h-screen">
       <Navbar />
       <main className="container">
+        <ScrollToTop/>
         <Outlet />
       </main>
       <Footer />
