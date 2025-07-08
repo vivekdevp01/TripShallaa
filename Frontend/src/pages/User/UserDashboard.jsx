f33d73adb26b6c093de9afe2055ea68c54947b3d
Harshit_Rawat <110838418+HarshitRawat2@users.noreply.github.com>
2025-07-08 14:50:12 +0530

    Update UserDashboard.jsx

--- Frontend/src/pages/User/UserDashboard.jsx
+++ Frontend/src/pages/User/UserDashboard.jsx
@@ -1,10 +1,11 @@
 import { HeroSection, ActivityCards } from "../../components/index.components.js"
-
+import FaqSection from "../../components/User/FaqSection.jsx";
 export default function UserDashboard() {
   return (
     <div>
       <HeroSection />
       <ActivityCards />
+      <FaqSection/>
     </div>
   );
 };
